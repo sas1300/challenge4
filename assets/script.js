73dc4b11e3e06309b92559903040e131d9f8d836
sas1300 <sascode13@gmail.com>
2020-09-26 20:22:12 -0500

    questions

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,42 @@
+//Questions
 
 var questions = [{
-    text: "how do you select a DOM element in javascript?",
-    answers: ["document.querySelector('id_name')", "tiffany", "rachel", "briffany"],
-    correctAnswer: "sheila"
-}, {
-    text: "what's your name?",
-    answers: ["sheila", "tiffany", "rachel", "briffany"],
-    correctAnswer: "sheila"
+
+    text: "How do you select a DOM element in javascript?",
+    answers: ["document.querySelector('id_name')", "document.questionSelector('id name')"],
+    correctAnswer: "document.querySelector('id_name')"
+}, 
+{   text: "What does HTML stand for?",
+    answers: ["Hyper Text Miricle Language", "Hyper Text Markup Language", "Heading Type Markup Language", "Hyper Text Markdown Language"],
+    correctAnswer: "Hyper Text Markup Language" 
+},
+
+ {  text: "Commonly used data types do not include:",
+    answers: ["Strings", "Booleans", "Alerts", "Numbers"],
+    correctAnswer: "Alerts"
+ },
+
+ {  text: "Arrays in JavaScript can be used to store:",
+    answers: ["Numbers and Strings", "Other arrays", "Booleans", "All of the above"], 
+    correctAnswer: "All of the above"
+},
+
+ {   text: "A useful tool used during developement and debugging for printing content to the debugger is:",
+     answers: ["JavaScript", "Terminal/Bash", "For Loops", "Console"],
+     correctAnswer: "Console"
+
+   },
+    {  text: "External stylesheets are stored in what type of files?",
+     answers: ["HTML", "PHP", "CSS", "XML"],
+     correctAnswer: "CSS"
 }]
 
+
 var buttonOne = document.querySelector("#buttonOne")
+var buttonTwo = document.querySelector("#buttonTwo")
+var buttonThree = document.querySelector("#buttonThree")
+var buttonFour = document.querySelector("#buttonFour")
+
 
 var position = 0
 score = 9
