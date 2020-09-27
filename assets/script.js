dc5960b1885751f6f7ade702d41b9c88bd808d1d
sas1300 <sascode13@gmail.com>
2020-09-26 21:08:16 -0500

    question buttons

--- assets/script.js
+++ assets/script.js
@@ -2,33 +2,30 @@
 
 var questions = [{
 
-    text: "How do you select a DOM element in javascript?",
-    answers: ["document.querySelector('id_name')", "document.questionSelector('id name')"],
-    correctAnswer: "document.querySelector('id_name')"
-}, 
-{   text: "What does HTML stand for?",
-    answers: ["Hyper Text Miricle Language", "Hyper Text Markup Language", "Heading Type Markup Language", "Hyper Text Markdown Language"],
-    correctAnswer: "Hyper Text Markup Language" 
+
+  text: "What does HTML stand for?",
+    answers: ["1. Hyper Text Miricle Language", "2. Hyper Text Markup Language", "3. Heading Type Markup Language", "4. Hyper Text Markdown Language"],
+    correctAnswer: "2. Hyper Text Markup Language" 
 },
 
  {  text: "Commonly used data types do not include:",
-    answers: ["Strings", "Booleans", "Alerts", "Numbers"],
-    correctAnswer: "Alerts"
+    answers: ["1. Strings", "2. Booleans", "3. Alerts", "4. Numbers"],
+    correctAnswer: "3. Alerts"
  },
 
  {  text: "Arrays in JavaScript can be used to store:",
-    answers: ["Numbers and Strings", "Other arrays", "Booleans", "All of the above"], 
-    correctAnswer: "All of the above"
+    answers: ["1. Numbers and Strings", "2. Other arrays", "3. Booleans", "4. All of the above"], 
+    correctAnswer: "4. All of the above"
 },
 
- {   text: "A useful tool used during developement and debugging for printing content to the debugger is:",
-     answers: ["JavaScript", "Terminal/Bash", "For Loops", "Console"],
-     correctAnswer: "Console"
+ {   text: "A useful tool used during developement and debugging is:",
+     answers: ["1. JavaScript", "2. Terminal/Bash", "3. For Loops", "4. Console"],
+     correctAnswer: "4. Console"
 
    },
     {  text: "External stylesheets are stored in what type of files?",
-     answers: ["HTML", "PHP", "CSS", "XML"],
-     correctAnswer: "CSS"
+     answers: ["1. HTML", "2. PHP", "3. CSS", "4. XML"],
+     correctAnswer: "3. CSS"
 }]
 
 
@@ -36,6 +33,7 @@ var buttonOne = document.querySelector("#buttonOne")
 var buttonTwo = document.querySelector("#buttonTwo")
 var buttonThree = document.querySelector("#buttonThree")
 var buttonFour = document.querySelector("#buttonFour")
+var buttonFour = document.querySelector("#buttonFive")
 
 
 var position = 0
@@ -55,7 +53,11 @@ document.querySelector("#startButton").addEventListener("click", function (event
 function questionUpdate() {
     document.querySelector("#questionText").innerHTML = questions[position].text;
     document.querySelector("#buttonOne").innerText = questions[position].answers[0];
-    //please write out the rest of the buttons on your own
+    document.querySelector("#buttonTwo").innerText = questions[position].answers[1];
+    document.querySelector("#buttonThree").innerText = questions[position].answers[2];
+    document.querySelector("#buttonFour").innerText = questions[position].answers[3];
+    document.querySelector("#buttonFive").innerText = questions[position].answers[4];
+    
     position++
 }
 
@@ -71,6 +73,12 @@ function makeAnswerButtonWork(button) {
 
 makeAnswerButtonWork(buttonOne)
 makeAnswerButtonWork(buttonTwo)
+makeAnswerButtonWork(buttonThree)
+makeAnswerButtonWork(buttonFour)
+makeAnswerButtonWork(buttonFive)
+
+
+
 //Button
 
 //timer
@@ -81,45 +89,3 @@ makeAnswerButtonWork(buttonTwo)
 
 //end game
 
-var string = "strings"
-
-var number = 9
-
-var float = 9.2323
-
-var numberAsAstring = "9"
-
-var array = ["1", "2", "3", "4", "5"]
-
-var object = {
-    taxes: "you owe too much",
-    fn: function (thing) {
-        console.log(thing)
-    },
-    object: {
-        string: string
-    }
-}
-
-function spaghettiMachine(dough) {
-    dough = dough + 1
-    return dough
-}
-
-var basket = spaghettiMachine(float)
-
-console.log(number)
-
-spaghettiMachine(number)
-
-console.log(number)
-
-console.log(object.taxes)
-
-console.log(array)
-
-for (i = 0; i < array.length; i++) {
-    array[i] = array[i] + array[i]
-}
-
-console.log(array)
